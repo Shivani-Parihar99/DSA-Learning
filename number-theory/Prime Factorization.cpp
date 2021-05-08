e3b7b75854ffb81eeb38d73e16838b2b56899463
Shivani-Parihar99 <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-05-08 18:31:39 +0530

    Create Prime Factorization.cpp

---
+++ number-theory/Prime Factorization.cpp
@@ -0,0 +1,26 @@
+// Given an integer, N, print all the prime factors of N.
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void print_prime_factors(int N){
+  for (int i = 2; i * i <= N; i++) {
+    if (N%i == 0){
+      cout << i << " ";
+      while (N%i == 0) 
+        N /= i;
+    }
+  }
+  if (N > 1)
+    cout << N << " ";
+  cout << "\n";
+}
+
+int main() {
+  ifstream cin("input.txt");
+
+  int N;
+  cin >> N;
+  print_prime_factors(N);
+  return 0;
+}
