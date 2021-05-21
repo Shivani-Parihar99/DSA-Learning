547b0ea41ddfe96b124db9bd455dac8dfb3da9ad
Shivani-Parihar99 <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-05-21 17:01:57 +0530

    Update and rename Find Immediate Greater Than X.cpp to Find Immediate Greater & smaller Than X.cpp

--- Arrays and vectors/Find Immediate Greater & smaller Than X.cpp
+++ Arrays and vectors/Find Immediate Greater & smaller Than X.cpp
@@ -15,3 +15,5 @@ int immediateGreater(int arr[], int n, int x){
         return ans;
         
     }
+
+/* Given an array arr[] of size N containing positive integers and an integer X, find the element in the array which is smaller than X and closest to it.
