742ea91364df0e3b6a7435245a787c7782eba2e5
Shivani-Parihar99 <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-06-07 22:31:48 +0530

    Create Hash.cpp

---
+++ Hashing/Hash.cpp
@@ -0,0 +1,2 @@
+/* Hash Function: A function that converts a given big phone number to a small practical integer value. The mapped integer value is used as an index in hash table.
+In simple terms, a hash function maps a big number or string to a small integer that can be used as index in hash table.*/
