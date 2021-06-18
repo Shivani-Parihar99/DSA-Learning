006706ed82522179567099cfe9447ccb431acdc5
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-06-18 22:23:55 +0530

    Update Open addressing.cpp

--- Hashing/Open addressing.cpp
+++ Hashing/Open addressing.cpp
@@ -1,1 +1,2 @@
-
+/*Like separate chaining, open addressing is a method for handling collisions. In Open Addressing, all elements are stored in the hash table itself.
+So at any point, the size of the table must be greater than or equal to the total number of keys (Note that we can increase table size by copying old data if needed). */
